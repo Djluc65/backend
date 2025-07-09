6743789e10835b15af2fc134497a4f78054fd221
Mackenson Jean Julien <mackensonjeanjulien65@gmail.com>
2025-07-09 16:28:31 +0200

    fixed res

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/movies', (req, res) => {
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}`)
     .then(response => response.json())
     .then(data => {
-        res.json({ movies: data });
+        res.json({ movies: data.results });
     });
 });
 
